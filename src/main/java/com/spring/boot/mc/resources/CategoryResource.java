9f307bfde15f6ce4cf48190a42e52937530e7a4a
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-16 11:40:42 -0300

    Testing the first domain class

--- src/main/java/com/spring/boot/mc/resources/CategoryResource.java
+++ src/main/java/com/spring/boot/mc/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.spring.boot.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.mc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is working";
+	public List<Category> list() {
+		List<Category> list = new ArrayList<>();
+		Category cat1 = new Category(1, "Books");
+		Category cat2 = new Category(2, "Tools");
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+		
 	}
 }
