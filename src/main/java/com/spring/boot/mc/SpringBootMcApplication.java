d8a0f2ef94b586bedc63fa6d5f1d58205f047cd7
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-16 10:25:35 -0300

    project created

---
+++ src/main/java/com/spring/boot/mc/SpringBootMcApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMcApplication.class, args);
+	}
+
+}
