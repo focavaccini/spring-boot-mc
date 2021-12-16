9a13e7f3b8fbb3d56ea19b57b3feb9f203d976a6
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-16 15:54:50 -0300

    Creating instantiation operation

--- src/main/java/com/spring/boot/mc/SpringBootMcApplication.java
+++ src/main/java/com/spring/boot/mc/SpringBootMcApplication.java
@@ -1,13 +1,33 @@
 package com.spring.boot.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootMcApplication {
+import com.spring.boot.mc.domain.Category;
+import com.spring.boot.mc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class SpringBootMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Books");
+		Category cat2 = new Category(null, "Tools");
+		Category cat3 = new Category(null, "Eletronics");
+		Category cat4 = new Category(null, "Computers");
+		Category cat5 = new Category(null, "Toys");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5));
 	}
 
 }
