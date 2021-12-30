28c2b6365f42450237d309879eddd42064773ecf
focavaccini <fernandocavaccini@gmail.com>
2021-12-30 15:17:33 -0300

    Custom validation: Email not repeated when entering the Client

--- src/main/java/com/spring/boot/mc/repositories/ClientRepository.java
+++ src/main/java/com/spring/boot/mc/repositories/ClientRepository.java
@@ -2,10 +2,12 @@ package com.spring.boot.mc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.boot.mc.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer>{
-
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/spring/boot/mc/services/validation/InsertClientValidator.java
+++ src/main/java/com/spring/boot/mc/services/validation/InsertClientValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.boot.mc.domain.Client;
 import com.spring.boot.mc.domain.enums.CustomerType;
 import com.spring.boot.mc.dto.ClientNewDTO;
+import com.spring.boot.mc.repositories.ClientRepository;
 import com.spring.boot.mc.resources.exception.FieldMessage;
 import com.spring.boot.mc.services.validation.utils.BR;
 
 public class InsertClientValidator implements ConstraintValidator<InsertClient, ClientNewDTO> {
+	
+	@Autowired
+	ClientRepository clientRepository;
+	
 	@Override
 	public void initialize(InsertClient ann) {
 	}
@@ -19,6 +27,11 @@ public class InsertClientValidator implements ConstraintValidator<InsertClient, 
 	@Override
 	public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
+		
+		Client aux = clientRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "This email already exists"));
+		}
 
 		if(objDto.getType().equals(CustomerType.PHYSICAL_PERSON.getCode()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "Invalid CPF"));
