cb474e7df36864d4701c9f43abf5df0d83d51f91
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-01-12 14:38:51 -0300

    Sending image by Endpoint

--- src/main/java/com/spring/boot/mc/config/SecurityConfig.java
+++ src/main/java/com/spring/boot/mc/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	};
 	
 	public static final String[] PUBLIC_MATCHERS_POST = {
-			"/clients/**",
+			"/clients/",
+			"/clients/picture",
 			"/auth/forgot/**"
 	};
 
--- src/main/java/com/spring/boot/mc/services/ClientService.java
+++ src/main/java/com/spring/boot/mc/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.spring.boot.mc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.spring.boot.mc.domain.Address;
 import com.spring.boot.mc.domain.City;
@@ -37,6 +39,9 @@ public class ClientService {
 	
 	@Autowired
 	private BCryptPasswordEncoder bCrypt;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Client findById(Integer id) {
 		
@@ -110,5 +115,9 @@ public class ClientService {
 	private void updateData(Client newObj, Client obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
